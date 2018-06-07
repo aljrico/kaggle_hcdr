06e255f54d14a6bfb847a9fb785fb7a1f102236c
Alejandro Jiménez <aljrico@gmail.com>
2018-06-07 21:20:34 +0200

    further report

--- na_replace.R
+++ na_replace.R
@@ -27,24 +27,21 @@ na_replace <- function(x){
 	if(is.vector(x) & !is.list(x)){
 		new_x <- x
 		w <- which(is.na(x))
-		n <- sum(is.na(x))
 		y <- x[!is.na(x)]
-		rep <- sample(x = y, size = length(w), replace = TRUE)
-		for(i in w) new_x[i] <- sample(x = y, size = 1, replace = TRUE)
+		for(i in w) new_x[i] <- sample(x = y, size = 1, replace = TRUE); cat(paste0("... ", floor(i/length(w)*100), "% ... \n"))
 		return(new_x)
 	}else if(is.data.frame(x)){
 		df <- as.data.frame(x)
 		ncols <- ncol(df)
 		for(i in 1:ncols){
-			x <- df[,i]
+			cat(paste0("... ", floor(i/ncols*100), "% ... \n"))
+			x <- df[i]
 			if(sum(is.na(x)) > 0){
 				new_x <- x
 				w <- which(is.na(x))
-				n <- sum(is.na(x))
 				y <- x[!is.na(x)]
-				rep <- sample(x = y, size = length(w), replace = TRUE)
-				for(i in w) new_x[i] <- sample(x = y, size = 1, replace = TRUE)
-				df[,i] <- new_x
+				for(k in w) new_x[k,] <- sample(x = y, size = 1, replace = TRUE)
+				df[i] <- new_x
 			}
 		}
 		return(df)
